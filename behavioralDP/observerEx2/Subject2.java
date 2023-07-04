ebbaedce957150c78542566f91bc652c862ddd3c
Suraj <surajsameer848506@gmail.com>
2023-07-04 21:36:21 +0530

    DesignPatterns

---
+++ behavioralDP/observerEx2/Subject2.java
@@ -0,0 +1,7 @@
+package behavioralDP.observerEx2;
+
+public interface Subject2 {
+    public void addObserver(Observer2 observer2);
+    public void removeObserver(Observer2 observer2);
+    public void notifyObserver();
+}
